d0895af7d2ac32106da407281d27ee88fd587334
John <johndoe@example.com>
2023-12-11 17:02:44 +0100

    partie3_1

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,23 @@
  * 
  *********************************************************************************/
 
+let imputEcriture = document.getElementById("inputEcriture")
+console.log(imputEcriture)
+
+let boutonValider = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+
+let zoneProposition = document.querySelector(".zoneProposition")
+console.log(zoneProposition)
+
+let zoneScore = document.querySelector(".zoneScore")
+console.log(zoneScore)
+
+
+let boutonsRadio = document.querySelectorAll(".optionSource input")
+console.log(boutonsRadio)
+
+
+
 lancerJeu()
